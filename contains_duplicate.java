cb64c59200068b0cecb1c9ee2508f5845b4bb205
Aditya Choudhary <adityachdhr555@gmail.com>
2025-05-09 09:45:21 +0530

    daily

---
+++ contains_duplicate.java
@@ -0,0 +1,11 @@
+class contains_duplicate{
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for(int i = 1; i < nums.length; i++){
+            if(nums[i] == nums[i - 1]){
+                return true;
+            }
+        }
+        return false;
+    }
+}
